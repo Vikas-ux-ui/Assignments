f666a2973177c5b673181f50dc25c261f7a38fcf
Vikas <140306201+Vikas-ux-ui@users.noreply.github.com>
2025-09-11 23:10:53 +0530

    Add files via upload

---
+++ C#/Assignment1-CurrencyConvertor/Assignment1-CurrencyConvertor/Program.cs
@@ -0,0 +1,16 @@
+﻿//Courency Covertor {   USD, CAD, Euro, GBP To INR  }
+
+Console.Write("Enter the amount (INR)");
+var inr = Console.ReadLine();//Reading INR amount 
+
+double INRamount = double.Parse(inr);//Parsing string to double
+
+Console.WriteLine("INR in USD"+INRamount * 88.22);//calculating USD
+Console.WriteLine("INR in Euro" + INRamount * 103.28);//Calculating INR in Euro
+Console.WriteLine("INR in CAD" + INRamount * 63.65);//Calculating INR in CAD
+Console.WriteLine("INR in GBP" + INRamount * 119.36);//Calculating INR in GBP
+
+
+ 
+
+
